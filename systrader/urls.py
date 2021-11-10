feedae838806371b8804a3dbb4a5f7d1e67c3746
quantylab <quantylab@gmail.com>
2021-11-10 04:39:21 +0000

    코드 정리

--- systrader/urls.py
+++ systrader/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from quantylab.systrader.creon import bridge_django
+from systrader import bridge_django
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- xing/__init__.py
+++
@@ -1,5 +0,0 @@
-from tslib.xing.res import parser
-
-
-XING_RES_PATH = "C:\\eBEST\\xingAPI\\Res\\"
-XING_RES_BLOCKS, XING_RES_OCCURS = parser.import_all()
